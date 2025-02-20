83cadf178d06a4c930c168ecde5e6f3228c3337a
Mateusz Sarnowski <sarnowskimateusz86@gmail.com>
2025-02-20 13:56:00 +0100

    update Readme

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,10 @@
 export function Footer() {
   return (
-    <footer className="container px-4 mx-auto">
-      <div className="flex justify-between items-center">
-        <p>Footer</p>
+    <footer className="container px-4 mx-auto py-12">
+      <div className="flex justify-center items-center">
+        <p className="text-gray-500 text-center">
+          Fikcyjny instytucja edukacyjna 2025
+        </p>
       </div>
     </footer>
   );
